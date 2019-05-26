1d01f79b1ec850a63b167a9845ba99dc39a77965
GreenDimond <24834740+GreenXenith@users.noreply.github.com>
2019-05-26 13:24:32 -0700

    Move ice cutting out of mod

--- misc.lua
+++ misc.lua
@@ -1,13 +1,3 @@
--- Add ice to the saw
-if minetest.get_modpath("moreblocks") then
-	stairsplus:register_all("ethereal", "ice", "default:ice", {
-		description = "Ice",
-		tiles = {"default_ice.png"},
-		groups = {cracky = 3, puts_out_fire = 1, cools_lava = 1},
-		sounds = default.node_sound_glass_defaults(),
-	})
-end
-
 --[[ Locked Bookshelf ]]--
 local bookshelf_formspec =
 	"size[8,7;]" ..
